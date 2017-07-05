9bdf077ed6c109a2a047a41077c42cd883ad45c1
Praktikum 2017 - Staircases <p17g3@i14pc190.anthropomatik.kit.edu>
2017-07-05 22:39:39 +0200

    added test script

--- tf-object-detection/README.md
+++ tf-object-detection/README.md
@@ -47,3 +47,22 @@ python ${PATH_TO_TENSORFLOW_MODEL_REPOSITORY}/object_detection/eval.py --logtost
 # use tensorboard to show evaluation progress
 tensorboard --logdir=eval_output
 ```
+
+# Exporting the trained model for inference
+```bash
+# From the stairs_detection/tf-object-detection directory
+# specify the checkpoint for inference export and create directory
+export CHECKPOINT_NUMBER=28276
+mkdir inference_output
+# export inverence graph
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+python ${PATH_TO_TENSORFLOW_MODEL_REPOSITORY}/object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path faster_rcnn_resnet101_cvhci.config --checkpoint_path train_output/model.ckpt-${CHECKPOINT_NUMBER} --inference_graph_path inference_output/output_inference_graph.pb
+```
+
+# Test the trained model with all images in the test_input directory
+```bash
+# From the stairs_detection/tf-object-detection directory
+# create output directory
+mkdir test_output
+python test.py
+```
---
+++ tf-object-detection/test.py
@@ -0,0 +1,72 @@
+import sys
+import os
+sys.path.append(os.path.join(os.environ['PATH_TO_TENSORFLOW_MODEL_REPOSITORY'], 'object_detection'))
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+
+PATH_TO_CKPT = 'inference_output/output_inference_graph.pb'
+PATH_TO_LABELS = 'data/cvhci_label_map.pbtxt'
+NUM_CLASSES = 1
+PATH_TO_TEST_IMAGES_DIR = 'test_input'
+TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, f) for f in os.listdir(PATH_TO_TEST_IMAGES_DIR) if os.path.isfile(os.path.join(PATH_TO_TEST_IMAGES_DIR, f))]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    i = 0
+    for image_path in TEST_IMAGE_PATHS:
+      image = Image.open(image_path)
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      image_np = load_image_into_numpy_array(image)
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      fig = plt.figure(figsize=IMAGE_SIZE)
+      plt.imshow(image_np)
+      fig.savefig('test_output/out'+str(i)+'.jpg')
+      i += 1
