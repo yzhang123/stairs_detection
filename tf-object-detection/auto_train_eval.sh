187156d9adf1b0df4c02e8dfdaf96f4d3070f3a3
yzhang <ujdsc@student.kit.edu>
2017-07-19 09:12:41 +0200

    added frame number to videos, raised score threshold for classification network, added graph argument to video_test.py

--- tf-object-detection/auto_train_eval.sh
+++ tf-object-detection/auto_train_eval.sh
@@ -2,11 +2,18 @@
 
 MAX_ITERATIONS=150000
 TRAIN_EVAL_STEPSIZE=5000
+CLASSIFICATION_SCORE_THRES=0.0
 
 mkdir -p /tmp/p17g3
 cp faster_rcnn_resnet101_cvhci.config /tmp/p17g3/faster_rcnn_resnet101_cvhci.config
 
 mkdir train_output_backup
+
+# set second_stage_post_processing score_threshold to CLASSIFICATION_SCORE_THRES, i.e. only boxes with scores above the specified value will
+# be regarded as positive
+
+sed -i -e "s/^        score_threshold: [0-9].[0-9]/        score_threshold: $CLASSIFICATION_SCORE_THRES/g" /tmp/p17g3/faster_rcnn_resnet101_cvhci.config
+
 
 for i in `seq $TRAIN_EVAL_STEPSIZE $TRAIN_EVAL_STEPSIZE $MAX_ITERATIONS`; do
   sed -i -e "s/^  num_steps: [0-9]*/  num_steps: $i/g" /tmp/p17g3/faster_rcnn_resnet101_cvhci.config
